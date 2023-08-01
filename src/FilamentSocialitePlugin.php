bcebb4557f48eb20744aac23a695386c2fb57c1d
Tom Janssen <tom@dcc.team>
2023-08-01 17:26:07 +0200

    wip

--- src/FilamentSocialitePlugin.php
+++ src/FilamentSocialitePlugin.php
@@ -7,6 +7,16 @@ use Filament\Panel;
 
 class FilamentSocialitePlugin implements Plugin
 {
+    public static function make(): static
+    {
+        return app(static::class);
+    }
+
+    public static function get(): static
+    {
+        return filament(static::make()->getId());
+    }
+
     public function getId(): string
     {
         return 'filament-socialite';
