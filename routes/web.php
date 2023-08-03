fcb1b4de7e3cd5f8d3e949ae5a4fbbaa465e2c40
Ramses Zeulevoet <ramses.zeulevoet@bagaar.be>
2023-08-03 09:13:30 +0200

    fix: update routes to use default panel domain and middleware

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,23 @@
 <?php
 
-Route::domain(config('filament.domain'))
-    ->middleware(config('filament.middleware.base'))
+use DutchCodingCompany\FilamentSocialite\Http\Controllers\SocialiteLoginController;
+use Filament\Facades\Filament;
+use Illuminate\Support\Facades\Route;
+
+$panel = Filament::getDefaultPanel();
+
+Route::domain($panel->getDomain())
+    ->middleware($panel->getMiddleWare())
     ->name('socialite.')
     ->group(function () {
         Route::get('/oauth/{provider}', [
-            \DutchCodingCompany\FilamentSocialite\Http\Controllers\SocialiteLoginController::class,
+            SocialiteLoginController::class,
             'redirectToProvider',
         ])
             ->name('oauth.redirect');
 
         Route::get('/oauth/callback/{provider}', [
-            \DutchCodingCompany\FilamentSocialite\Http\Controllers\SocialiteLoginController::class,
+            SocialiteLoginController::class,
             'processCallback',
         ])
             ->name('oauth.callback');
