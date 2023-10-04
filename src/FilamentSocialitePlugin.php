005808db1c195d15499b2efcc550bcbc29d08af0
Bram Raaijmakers <b.raaijmakers@hotmail.com>
2023-10-04 16:11:32 +0200

    Set providers.

--- src/FilamentSocialitePlugin.php
+++ src/FilamentSocialitePlugin.php
@@ -7,6 +7,8 @@ use Filament\Panel;
 
 class FilamentSocialitePlugin implements Plugin
 {
+    protected array $providers = [];
+
     public static function make(): static
     {
         return app(static::class);
@@ -30,5 +32,17 @@ class FilamentSocialitePlugin implements Plugin
     public function boot(Panel $panel): void
     {
         //
+    }
+
+    public function setProviders(array $providers): static
+    {
+        $this->providers = $providers;
+
+        return $this;
+    }
+
+    public function getProviders(): array
+    {
+        return $this->providers;
     }
 }
--- src/Http/Livewire/Buttons.php
+++ src/Http/Livewire/Buttons.php
@@ -2,7 +2,7 @@
 
 namespace DutchCodingCompany\FilamentSocialite\Http\Livewire;
 
-use DutchCodingCompany\FilamentSocialite\Facades\FilamentSocialite;
+use Filament\Facades\Filament;
 use Livewire\Component;
 
 class Buttons extends Component
@@ -10,7 +10,9 @@ class Buttons extends Component
     public function render()
     {
         return view('filament-socialite::livewire.buttons', [
-            'providers' => FilamentSocialite::getProviderButtons(),
+            'providers' => Filament::getCurrentPanel()
+                ->getPlugin('filament-socialite')
+                ->getProviders(),
         ]);
     }
 }
