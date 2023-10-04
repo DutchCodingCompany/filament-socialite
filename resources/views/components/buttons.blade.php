9ce768303207e43c93e8402dedf32394e2cfeed6
Bram Raaijmakers <bram@dutchcodingcompany.com>
2023-10-04 11:11:31 +0200

    Update buttons html.

--- resources/views/components/buttons.blade.php
+++ resources/views/components/buttons.blade.php
@@ -1,23 +1,25 @@
-@if (count($providers))
-    <div class="relative flex items-center justify-center text-center">
-        <div class="absolute border-t border-gray-200 w-full h-px"></div>
-        <p class="inline-block relative bg-white text-sm p-2 rounded-full font-medium text-gray-500 dark:bg-gray-800 dark:text-gray-100">
-            {{ __('filament-socialite::auth.login-via') }}
-        </p>
-    </div>
+<div>
+    @if (count($providers))
+        <div class="relative flex items-center justify-center text-center">
+            <div class="absolute border-t border-gray-200 w-full h-px"></div>
+            <p class="inline-block relative bg-white text-sm p-2 rounded-full font-medium text-gray-500 dark:bg-gray-800 dark:text-gray-100">
+                {{ __('filament-socialite::auth.login-via') }}
+            </p>
+        </div>
 
-    <div class="grid @if(count($providers) > 1) grid-cols-2 @endif gap-4">
-        @foreach($providers as $key => $provider)
-            <x-filament::button
-                color="secondary"
-                :icon="$provider['icon'] ?? null"
-                tag="a"
-                :href="route('socialite.oauth.redirect', $key)"
-            >
-                {{ $provider['label'] }}
-            </x-filament::button>
-        @endforeach
-    </div>
-@else
-    <span></span>
-@endif
+        <div class="grid @if(count($providers) > 1) grid-cols-2 @endif gap-4">
+            @foreach($providers as $key => $provider)
+                <x-filament::button
+                    color="secondary"
+                    :icon="$provider['icon'] ?? null"
+                    tag="a"
+                    :href="route('socialite.oauth.redirect', $key)"
+                >
+                    {{ $provider['label'] }}
+                </x-filament::button>
+            @endforeach
+        </div>
+    @else
+        <span></span>
+    @endif
+</div>
