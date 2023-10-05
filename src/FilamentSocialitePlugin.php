f95eb6bdb8b672f7bb619ededae4aa730cf311c6
Bram Raaijmakers <b.raaijmakers@hotmail.com>
2023-10-05 16:04:42 +0200

    Add getters and setters

--- src/FilamentSocialitePlugin.php
+++ src/FilamentSocialitePlugin.php
@@ -11,7 +11,15 @@ class FilamentSocialitePlugin implements Plugin
 
     protected string $loginRouteName = 'filament.admin.auth.login';
 
+    protected string $dashboardRouteName = 'filament.admin.pages.dashboard';
+
     protected bool $rememberLogin = false;
+
+    protected bool $registrationEnabled = false;
+
+    protected array $domainAllowList = [];
+
+    protected string $userModelClass = \App\Models\User::class;
 
     public static function make(): static
     {
@@ -50,9 +58,9 @@ class FilamentSocialitePlugin implements Plugin
         return $this->providers;
     }
 
-    public function setLoginRouteName(string $loginRouteName): static
+    public function setLoginRouteName(string $value): static
     {
-        $this->loginRouteName = $loginRouteName;
+        $this->loginRouteName = $value;
 
         return $this;
     }
@@ -60,6 +68,18 @@ class FilamentSocialitePlugin implements Plugin
     public function getLoginRouteName(): string
     {
         return $this->loginRouteName;
+    }
+
+    public function setDashboardRouteName(string $value): static
+    {
+        $this->dashboardRouteName = $value;
+
+        return $this;
+    }
+
+    public function getDashboardRouteName(): string
+    {
+        return $this->dashboardRouteName;
     }
 
     public function setRememberLogin(bool $value): static
@@ -72,5 +92,41 @@ class FilamentSocialitePlugin implements Plugin
     public function getRememberLogin(): bool
     {
         return $this->rememberLogin;
+    }
+
+    public function setRegistrationEnabled(bool $value): static
+    {
+        $this->registrationEnabled = $value;
+
+        return $this;
+    }
+
+    public function getRegistrationEnabled(): bool
+    {
+        return $this->registrationEnabled;
+    }
+
+    public function setDomainAllowList(array $values): static
+    {
+        $this->domainAllowList = $values;
+
+        return $this;
+    }
+
+    public function getDomainAllowList(): array
+    {
+        return $this->domainAllowList;
+    }
+
+    public function setUserModelClass(string $value): static
+    {
+        $this->userModelClass = $value;
+
+        return $this;
+    }
+
+    public function getUserModelClass(): string
+    {
+        return $this->userModelClass;
     }
 }
