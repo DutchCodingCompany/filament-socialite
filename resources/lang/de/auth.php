4f3840f27cc5d0af8863483a9db85a70a0aed048
ArtrenH <79058115+ArtrenH@users.noreply.github.com>
2024-03-03 01:10:42 +0100

    german translations

---
+++ resources/lang/de/auth.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'login-via' => 'Oder login über',
+
+    'login-failed' => 'Login fehlgeschlagen, bitte versuchen erneut versuchen.',
+
+    'user-not-allowed' => 'Ihre E-Mail-Adresse hat keine erlaubte Domain.',
+
+    'registration-not-enabled' => 'Registrierung eines neuen Benutzers ist nicht erlaubt.',
+];
