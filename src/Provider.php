131fb305eee9fc4095b1d968aa056317092ebf02
Bert Wijnhoven <bertwijnhoven@hotmail.com>
2024-04-12 13:48:48 +0200

    allow empty icon

--- src/Provider.php
+++ src/Provider.php
@@ -15,7 +15,7 @@ class Provider
 
     protected string $label;
 
-    protected string | null $icon;
+    protected string | null $icon = null;
 
     /**
      * @var string | array{50: string, 100: string, 200: string, 300: string, 400: string, 500: string, 600: string, 700: string, 800: string, 900: string, 950: string} | null
