79ce8ece4124dd77cf67ee9bd54465acae7d4003
Carlos Andres Perez Ubeda <descarlos2013@gmail.com>
2024-10-28 23:48:11 -0600

    feat: Add Spanish localization for authentication messages
    Created a new auth.php file under resources/lang/es to provide Spanish translations for authentication related messages. The translations include messages for login methods, failed login attempts, unauthorized email notifications, and disabled user registrations.

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'login-via' => 'O inicie sesión a través de',
+
+    'login-failed' => 'El inicio de sesión falló, por favor inténtelo de nuevo.',
+
+    'user-not-allowed' => 'Su correo electrónico no está autorizado para ingresar.',
+
+    'registration-not-enabled' => 'No está permitido el registro de un nuevo usuario.',
+];
