6ea2d70f98ad770a1f6353ceaa131eb75660ef94
Baspa <hello@baspa.dev>
2024-11-22 09:14:27 +0100

    Add Dutch translations

---
+++ resources/lang/nl/auth.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'login-via' => 'Of log in via',
+
+    'login-failed' => 'Inloggen mislukt, probeer het opnieuw.',
+
+    'user-not-allowed' => 'Uw e-mailadres maakt geen deel uit van een toegestaan domein.',
+
+    'registration-not-enabled' => 'Registratie van een nieuwe gebruiker is niet toegestaan.',
+];
