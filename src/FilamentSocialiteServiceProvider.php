50076b5e48598d15b1e2e443dcc925fbc372f8fe
Tom Janssen <tom@dcc.team>
2025-02-21 12:25:40 +0100

    fix

--- src/FilamentSocialiteServiceProvider.php
+++ src/FilamentSocialiteServiceProvider.php
@@ -49,7 +49,10 @@ class FilamentSocialiteServiceProvider extends PackageServiceProvider
             },
         );
 
-        if (version_compare(app()->version(), '11.0', '>=')) {
+        if (
+            version_compare(app()->version(), '11.0', '>=')
+            && method_exists(VerifyCsrfToken::class, 'except')
+        ) {
             VerifyCsrfToken::except([
                 '*/oauth/callback/*',
                 'oauth/callback/*',
