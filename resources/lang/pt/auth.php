8e6b48f683d891635e260c3d8bb065d09f2e25e8
victor <victor@brain-j.co.jp>
2025-03-22 03:24:50 +0900

    add support to pt (portugal) lang

---
+++ resources/lang/pt/auth.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'login-via' => 'Ou faça login através de',
+
+    'login-failed' => 'O acesso falhou, por favor tente novamente.',
+
+    'user-not-allowed' => 'O seu e-mail não pertence a um domínio permitido.',
+
+    'registration-not-enabled' => 'Não está permitido o registo de novos utilizadores.',
+];
