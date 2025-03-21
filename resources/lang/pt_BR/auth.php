7f954d8765b6d9480bc13367a01d5f39879f3613
victor <victor@brain-j.co.jp>
2025-03-22 03:27:48 +0900

    add support to pt_BR (Brazil) lang

---
+++ resources/lang/pt_BR/auth.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'login-via' => 'Ou faça login via',
+
+    'login-failed' => 'Falha no login, tente novamente.',
+
+    'user-not-allowed' => 'Seu e-mail não faz parte de um domínio permitido.',
+
+    'registration-not-enabled' => 'Não é permitido o registro de novos usuários.',
+];
