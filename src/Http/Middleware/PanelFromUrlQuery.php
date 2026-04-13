c48beea64e9ef51c0c64777e5fe8491a6f02835c
Olaf <olaf@fului.com>
2026-04-13 20:50:38 +0200

    fix: update state retrieval method in PanelFromUrlQuery middleware
    Symfony has deprecated the direct `get()` method.
    https://symfony.com/blog/new-in-symfony-7-4-request-class-improvements
    
    This should fix the following warning.
    
    ```
    Since symfony/http-foundation 7.4: Request::get() is deprecated,
    use properties ->attributes, query or request directly instead. in
    /var/www/vendor/symfony/deprecation-contracts/function.php on line 25
    ```

--- src/Http/Middleware/PanelFromUrlQuery.php
+++ src/Http/Middleware/PanelFromUrlQuery.php
@@ -33,11 +33,11 @@ class PanelFromUrlQuery
     public static function decrypt(Request $request): string
     {
         try {
-            if (! is_string($request->get('state'))) {
+            if (! is_string($request->query->get('state'))) {
                 throw new DecryptException('State is not a string.');
             }
 
-            return Crypt::decrypt($request->get('state'));
+            return Crypt::decrypt($request->query->get('state'));
         } catch (DecryptException $e) {
             throw InvalidCallbackPayload::make($e);
         }
